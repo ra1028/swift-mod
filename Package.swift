633a3ec7efebf066da97d0eba04aa5cab08af400
Ryo Aoyama <r.fe51028.r@gmail.com>
2020-07-20 21:49:11 +0900

    Update yams

--- Package.swift
+++ Package.swift
@@ -51,7 +51,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", .revision("swift-5.3-DEVELOPMENT-SNAPSHOT-2020-07-04-a")),
         .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.1.0"),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0")
+        .package(url: "https://github.com/jpsim/Yams.git", from: "3.0.1")
     ],
     targets: [
         cli,
