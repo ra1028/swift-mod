4accd0639f20eb50f229c4fd251a1bd26325e59d
Ras <ras.suleimenov@mercari.com>
2024-11-21 15:57:54 +0900

    Bump tools

--- Tools/Package.swift
+++ Tools/Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
     name: "Tools",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-format.git", .upToNextMinor(from: "509.0.0")),
+        .package(url: "https://github.com/apple/swift-format.git", .upToNextMinor(from: "510.1.0")),
     ]
 )
